d04189ebfb652accbbde009684c91fa796fc5792
JJY <475108923@qq.com>
2018-05-12 00:56:17 +0800

    update 344,476,500,557,791,821

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
 import extra.E01_FullPermutation;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String args[]) {
@@ -39,20 +37,44 @@ public class Main {
 //        Map<String, Integer> map = new HashMap<>();
 //        System.out.println(map.get("1"));
 
-        String s = "123 jifewofji";
-        long t1 = timecountIterable(() -> {
-            String[] split = s.split(" ");
-            int I = Integer.parseInt(split[0]);
-            String S = split[1];
-        }, 10000000);
+//        String s = "123 jifewofji";
+//        long t1 = timecountIterable(() -> {
+//            String[] split = s.split(" ");
+//            int I = Integer.parseInt(split[0]);
+//            String S = split[1];
+//        }, 10000000);
+//
+//        long t2 = timecountIterable(() -> {
+//            int index = s.indexOf(" ");
+//            int I = Integer.parseInt(s.substring(0, index));
+//            String S = s.substring(index + 1);
+//        }, 10000000);
+//
+//        System.out.println(t1 + ", " + t2);
 
-        long t2 = timecountIterable(() -> {
-            int index = s.indexOf(" ");
-            int I = Integer.parseInt(s.substring(0, index));
-            String S = s.substring(index + 1);
-        }, 10000000);
+//        System.out.println((~5));
+//        System.out.println(Integer.toBinaryString(5));
+//        System.out.println(Integer.toBinaryString(-6));
+//        System.out.println(~5 & 7);
+//        System.out.println(~5 & (1 << 3) - 1);
 
-        System.out.println(t1 + ", " + t2);
+//        System.out.println("1c2adb3".indexOf('c', -1));
+//        System.out.println(new P557_ReverseWordsInAStringIII().new Solution().reverseWords("hello world"));
+//        Set<String> stringSet = Collections.emptySet();
+//        stringSet.add("hje");
+//        System.out.println(stringSet);
+//        char[] chars = {'a', 'b'};
+//        System.out.println(Arrays.asList(chars));
+
+//        String s = "asdb";
+//        s.toCharArray()[0] = 3;
+//        System.out.println(s);
+
+        String S = "kqep";
+        String T = "pekeq";
+        System.out.println("S: " + S + ", T: " + T);
+        String res = new P791_CustomSortString().new Solution().customSortString(S, T);
+        System.out.println("Result: " + res);
     }
 
     private static long timecount(Runnable runnable) {
