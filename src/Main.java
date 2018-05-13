c6e1e80936195ad37d5492f30c11d3d2d2fc2cd9
JJY <475108923@qq.com>
2018-05-13 12:07:10 +0800

    update 682

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
-import extra.E01_FullPermutation;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
 
 public class Main {
     public static void main(String args[]) {
@@ -38,13 +39,13 @@ public class Main {
 //        System.out.println(map.get("1"));
 
 //        String s = "123 jifewofji";
-//        long t1 = timecountIterable(() -> {
+//        long t1 = timecountLoop(() -> {
 //            String[] split = s.split(" ");
 //            int I = Integer.parseInt(split[0]);
 //            String S = split[1];
 //        }, 10000000);
 //
-//        long t2 = timecountIterable(() -> {
+//        long t2 = timecountLoop(() -> {
 //            int index = s.indexOf(" ");
 //            int I = Integer.parseInt(s.substring(0, index));
 //            String S = s.substring(index + 1);
@@ -76,23 +77,121 @@ public class Main {
 //        String res = new P791_CustomSortString().new Solution().customSortString(S, T);
 //        System.out.println("Result: " + res);
 
-        int d = new P461_HammingDistance().new Solution().hammingDistance(3, 1);
-        System.out.println(d);
+//        int d = new P461_HammingDistance().new Solution().hammingDistance(3, 1);
+//        System.out.println(d);
+
+//        timecountLoop(() -> {
+//            int r = 512;
+//            while (r > 0) {
+//                if (flag) {
+//                    System.out.print(r + ", ");
+//                }
+//                r /= 2;
+//            }
+//            flag = false;
+//        }, 100000000);
+//
+//        flag = true;
+//        timecountLoop(() -> {
+//            int r = 512;
+//            for (int i = 1; r > 0; i++) {
+//                if (flag) {
+//                    System.out.print(r + ", ");
+//                }
+//                r = 512 >> i;
+//            }
+//            flag = false;
+//        }, 100000000);
+//        flag = true;
+//        timecountLoop(() -> {
+//            int r = 512;
+//            while (r > 0) {
+//                if (flag) {
+//                    System.out.print(r + ", ");
+//                }
+//                r >>= 1;
+//            }
+//            flag = false;
+//        }, 100000000);
+//
+//
+//        timecountLoop(() -> {}, 100000000);
+//        timecountLoop(new Runnable() {
+//            @Override
+//            public void run() {
+//            }
+//        }, 100000000);
+
+//        System.out.println(Integer.MAX_VALUE);
+//        List<Integer> randomList = randomIntegerList(1000000);
+//
+//        timecount(() -> {
+//            max = Integer.MIN_VALUE;
+//            for (int i = 0; i < randomList.size(); i++) {
+//                int aInt = randomList.get(i);
+//                if (aInt > max) {
+//                    max = aInt;
+//                }
+//            }
+//            System.out.println("max = " + max);
+//        });
+//        timecount(() -> {
+//            max = Integer.MIN_VALUE;
+//            for (int i : randomList) {
+//                if (i > max) {
+//                    max = i;
+//                }
+//            }
+//            System.out.println("max = " + max);
+//        });
+//        timecount(() -> {
+//            max = Integer.MIN_VALUE;
+//            Iterator<Integer> it = randomList.iterator();
+//            while (it.hasNext()) {
+//                int aInt = it.next();
+//                if (aInt > max) {
+//                    max = aInt;
+//                }
+//            }
+//            System.out.println("max = " + max);
+//        });
+        String ops[] = {"5", "2", "2", "2", "2", "2", "2", "2", "2", "C", "D", "C", "C", "C", "C", "C", "C"};
+        int sum = new P682_BaseballGame().new Solution().calPoints(ops);
+        System.out.println(sum);
     }
+
+    private static int max;
+
+    static boolean flag = true;
 
     private static long timecount(Runnable runnable) {
         long start = System.currentTimeMillis();
         runnable.run();
         long end = System.currentTimeMillis();
-        return end - start;
+        long use = end - start;
+        System.out.println("use " + use + "ms");
+        return use;
     }
 
-    private static long timecountIterable(Runnable runnable, int count) {
+    private static long timecountLoop(Runnable runnable, int count) {
         long start = System.currentTimeMillis();
         for (int i = 0; i < count; i++) {
             runnable.run();
         }
         long end = System.currentTimeMillis();
-        return end - start;
+        long use = end - start;
+        System.out.println("use " + use + "ms");
+        return use;
+    }
+
+    private static List<Integer> randomIntegerList(int size) {
+        List<Integer> result = new ArrayList<>(size);
+
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+            result.add(random.nextInt());
+        }
+        return result;
     }
 }
