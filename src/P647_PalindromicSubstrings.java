79807fb1c1fd34b8bb03dfd9c75e56fffd324be8
JJY <475108923@qq.com>
2018-06-10 20:24:09 +0800

    update 413

--- src/P647_PalindromicSubstrings.java
+++ src/P647_PalindromicSubstrings.java
@@ -1,4 +1,7 @@
 public class P647_PalindromicSubstrings {
+    /**
+     * 暴力且低效
+     */
     class Solution {
         public int countSubstrings(String s) {
             if (s == null || s.length() == 0) {
@@ -6,22 +9,31 @@ public class P647_PalindromicSubstrings {
             } else if (s.length() == 1) {
                 return 1;
             }
-            char[] arr = s.toCharArray();
-            char lastChar = arr[0];
-            int res = 1;
-            int repeat = 1;
-
-            for (int i = 1; i < arr.length; i++) {
-                if (arr[i] != lastChar) {
-                    res++;
-                    repeat = 1;
-                    lastChar = arr[i];
-                } else {
-                    repeat++;
-                    res += repeat;
+            int count = 0;
+            int len = s.length();
+            for (int i = 0; i < len; i++) {
+                for (int j = i + 1; j <= len; j++) {
+                    String sub = s.substring(i, j);
+                    if (isPalindromic(sub)) {
+                        count++;
+                    }
                 }
             }
-            return res;
+
+            return count;
+        }
+
+        public boolean isPalindromic(String s) {
+            char[] toCharArr = s.toCharArray();
+            int len = toCharArr.length;
+            int end = len / 2;
+
+            for (int i = 0; i < end; i++) {
+                if (toCharArr[i] != toCharArr[len - 1 - i]) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
