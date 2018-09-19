342564e741506839a447b17d7aacb61e03a3b60a
jiangjy <jiangjingyi@cleartv.cn>
2018-09-19 17:37:35 +0800

    update todo P885

--- src/P885_SpiralMatrixIII.java
+++ src/P885_SpiralMatrixIII.java
@@ -1,7 +1,19 @@
 public class P885_SpiralMatrixIII {
     class Solution {
         public int[][] spiralMatrixIII(int R, int C, int r0, int c0) {
-            
+
+        }
+
+        private int[] nextCorner(int[] center, int[] last) {
+            int[] next = new int[2];
+            int lastX = last[0];
+            int lastY = last[1];
+            int centerX = center[0];
+            int centerY = center[1];
+            if (lastX > centerX && lastY <= centerY) {
+                // Northeast
+                next[0]=
+            }
         }
     }
 }
