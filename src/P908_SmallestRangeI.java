658a4c467c71c90106d01c747ed2848be9abd449
jiangjy <jiangjingyi@cleartv.cn>
2018-09-27 19:38:27 +0800

    update problem 885, add problem 590, 700, 701, 908

---
+++ src/P908_SmallestRangeI.java
@@ -0,0 +1,13 @@
+public class P908_SmallestRangeI {
+    class Solution {
+        public int smallestRangeI(int[] A, int K) {
+            int max, min;
+            max = min = A[0];
+            for (int a : A) {
+                if (a > max) max = a;
+                if (a < min) min = a;
+            }
+            return max - min > K * 2 ? max - min - K * 2 : 0;
+        }
+    }
+}
