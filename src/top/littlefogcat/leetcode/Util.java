273f91a003cf864090679fde826c5f0997e60280
littlefogcat <littlefogcat@foxmail.com>
2020-07-12 04:58:25 +0800

    update 21-合并两个有序链表、174-地下城游戏、315-计算数组右侧小于当前元素的个数

--- src/top/littlefogcat/leetcode/Util.java
+++ src/top/littlefogcat/leetcode/Util.java
@@ -1,5 +1,10 @@
 package top.littlefogcat.leetcode;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
 public class Util {
 
     public static void print(Object... args) {
@@ -37,6 +42,60 @@ public class Util {
         return head;
     }
 
+    public static boolean isNumber(char c) {
+        return c >= '0' && c <= '9';
+    }
+
+    /**
+     * 读取文件，需要文件内容是一个int数组
+     */
+    public static int[] readIntArrayFromFile(String file) {
+        String fileName = "files/" + file;
+        ArrayList<Integer> arr = new ArrayList<>();
+        try (InputStreamReader reader = new InputStreamReader(new FileInputStream(fileName))) {
+            int ch;
+            int num = 0;
+            boolean flag = false; // 标记是否正在读一个数
+            int neg = 1; // 负数标记
+            while ((ch = reader.read()) != -1) {
+                char c = (char) ch;
+                if (flag) {
+                    if (isNumber(c)) {
+                        num = num * 10 + (c - '0');
+                    } else {
+                        if (c == ' ' || c == ',') {
+                            flag = false;
+                            arr.add(num * neg);
+                        } else if (c == ']') {
+                            arr.add(num * neg);
+                            break;
+                        } else throw new RuntimeException("文件格式错误");
+                    }
+                } else {
+                    // 开始读一个数的第一位
+                    if (c == '-') {
+                        flag = true;
+                        num = 0;
+                        neg = -1;
+                    } else if (isNumber(c)) {
+                        flag = true;
+                        num = c - '0';
+                        neg = 1;
+                    } else if (c == ']') {
+                        break;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int[] a = new int[arr.size()];
+        for (int i = 0; i < arr.size(); i++) {
+            a[i] = arr.get(i);
+        }
+        return a;
+    }
+
     public static class ConvertQuoteSymbol {
 
         /**
@@ -49,7 +108,7 @@ public class Util {
         }
 
         public static void main(String[] args) {
-            String arr = "[-1, 0, 1, 2, -1, -4]";
+            String arr = "[[-2,-3,3],[-5,-10,1],[10,30,-5]]";
             System.out.println(convertFangkuohaoToHuakuohao(arr));
         }
     }
